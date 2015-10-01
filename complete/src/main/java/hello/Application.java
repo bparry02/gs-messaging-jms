0bf3479a6222a83bdae69bd7c1d7c0a1cf632087
Bryan Parry <bparry02@gmail.com>
2015-09-30 22:48:44 -0400

    Change to use remote HornetQ JMS via JBoss EAP
    Quickstart modified to connect to a remote HornetQ connection
    factory via JNDI.

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,12 +1,17 @@
-
 package hello;
 
-import java.io.File;
+import java.util.Properties;
+
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -16,26 +21,84 @@ import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
-import org.springframework.util.FileSystemUtils;
+import org.springframework.jms.support.destination.DestinationResolver;
+import org.springframework.jms.support.destination.JndiDestinationResolver;
 
 @SpringBootApplication
 @EnableJms
 public class Application {
 
+    private static final Logger LOG = LoggerFactory
+            .getLogger(Application.class);
 
-    @Bean // Strictly speaking this bean is not necessary as boot creates a default
-    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+    private static final String DEFAULT_CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+    public static final String DEFAULT_DESTINATION = "java:/jms/helloBoot";
+    private static final String DEFAULT_USERNAME = "boot";
+    private static final String DEFAULT_PASSWORD = "bootUser1!";
+    private static final String INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+    private static final String PROVIDER_URL = "remote://localhost:4447";
+
+    @Bean
+    JmsListenerContainerFactory<?> myJmsContainerFactory(
+            ConnectionFactory connectionFactory,
+            DestinationResolver destinationResolver) {
         SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
+        factory.setDestinationResolver(destinationResolver);
         return factory;
+    }
+
+    @Bean
+    DestinationResolver destinationResolver(Properties jndiEnv) {
+        JndiDestinationResolver resolver = new JndiDestinationResolver();
+        resolver.setJndiEnvironment(jndiEnv);
+        return resolver;
+    }
+
+    @Bean
+    Properties jndiEnv() {
+        Properties env = new Properties();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+        env.put(Context.PROVIDER_URL,
+                System.getProperty(Context.PROVIDER_URL, PROVIDER_URL));
+        env.put(Context.SECURITY_PRINCIPAL,
+                System.getProperty("username", DEFAULT_USERNAME));
+        env.put(Context.SECURITY_CREDENTIALS,
+                System.getProperty("password", DEFAULT_PASSWORD));
+        // NOTE: also chose to disable security in hornetq so that this user can
+        // connect and is authorized to publish.
+        return env;
+    }
+
+    @Bean
+    InitialContext initialContext(Properties jndiEnv) {
+        try {
+            return new InitialContext(jndiEnv);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Bean
+    ConnectionFactory connectionFactory(InitialContext context) {
+        try {
+            LOG.info("looking up ConnectionFactory....");
+            return (ConnectionFactory) context
+                    .lookup(DEFAULT_CONNECTION_FACTORY);
+        } catch (NamingException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public static void main(String[] args) {
         // Clean out any ActiveMQ data from a previous run
-        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+        // FileSystemUtils.deleteRecursively(new File("activemq-data"));
 
         // Launch the application
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(
+                Application.class, args);
 
         // Send a message
         MessageCreator messageCreator = new MessageCreator() {
@@ -46,7 +109,7 @@ public class Application {
         };
         JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
         System.out.println("Sending a new message.");
-        jmsTemplate.send("mailbox-destination", messageCreator);
+        jmsTemplate.send(DEFAULT_DESTINATION, messageCreator);
     }
 
 }
--- complete/src/main/java/hello/Receiver.java
+++ complete/src/main/java/hello/Receiver.java
@@ -1,12 +1,9 @@
 package hello;
 
-import java.io.File;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
-import org.springframework.util.FileSystemUtils;
 
 @Component
 public class Receiver {
@@ -21,10 +18,10 @@ public class Receiver {
      * When you receive a message, print it out, then shut down the application.
      * Finally, clean up any ActiveMQ server stuff.
      */
-    @JmsListener(destination = "mailbox-destination", containerFactory = "myJmsContainerFactory")
+    @JmsListener(destination = Application.DEFAULT_DESTINATION, containerFactory = "myJmsContainerFactory")
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        context.close();
-        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+//        context.close();
+//        FileSystemUtils.deleteRecursively(new File("activemq-data"));
     }
 }
